c4c0a2ac5acea63d0339887b650fd12f59903e73
Martha Gonzales Chumacero <marthagonzales.sis24ch@tecba.edu.bo>
2024-10-24 00:30:07 -0400

    Cambios realizados en JS

---
+++ script.js
@@ -0,0 +1,10 @@
+//si un numero esta en el rango de 10 a 15
+//18 == no esta en el rango , 12 == estas en el rango
+
+let numero = 11;
+
+if (numero >= 10 && numero <= 15) {
+    console.log("si estas en el rango");
+} else {
+    console.log("no estas en el rango");
+}
